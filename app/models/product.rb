71f7f8d67658f9f9941964fd0c27f5bc9a1fc5fb
Radek <radekgiz@gmail.com>
2014-03-24 14:32:27 +0100

    poprawiona validacja

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,4 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence => true
 	validates :price, :numericality => {:greater_than_or_equak_to => 0.01}
 	validates :title, :uniqueness => true
-	validates jdkfj 
 end
