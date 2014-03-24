5a9045298038ff1ee7997d4f4acf5a724803ae64
Radek <radekgiz@gmail.com>
2014-03-24 14:31:26 +0100

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence => true
 	validates :price, :numericality => {:greater_than_or_equak_to => 0.01}
-	validates :title, :uniqueness => true 
+	validates :title, :uniqueness => true
+	validates jdkfj 
 end
